539a8364de40c1794cbea4f5899d7db64c611094
hatemrihan <your-email@example.com>
2025-05-18 22:01:49 +0300

    modify faqs

--- app/sections/Faqs.tsx
+++ app/sections/Faqs.tsx
@@ -94,26 +94,7 @@ const FAQs = () => {
               }}
               transition={{ delay: 0.5, duration: 0.5 }}
             >
-              <a
-                href="#"
-                className="text-black dark:text-white inline-flex items-center font-medium"
-              >
-                More FAQs
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 ml-1"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 5l7 7-7 7"
-                  />
-                </svg>
-              </a>
+             
             </motion.div>
           </motion.div>
 
